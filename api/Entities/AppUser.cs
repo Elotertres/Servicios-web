823a49224209d29ad738c3a379b9bb6d21dc4985
Juan Pablo Martinez Rocha <Elotertres@hotmail.com>
2024-10-06 17:43:53 -0600

    Parcial1 - Add pasword

--- api/Entities/AppUser.cs
+++ api/Entities/AppUser.cs
@@ -4,5 +4,7 @@ namespace api.Entities
     {
         public int Id { get; set; }
         public required string UserName { get; set;}
+        public required byte[] passwordHash { get; set;}
+        public required byte[] passwordSalt { get; set; }
     }
 }
