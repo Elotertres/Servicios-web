8e3c2c0c18dd8ee0d795d7e70adea40345340b7c
Juan Pablo Martinez Rocha <Elotertres@hotmail.com>
2024-10-06 19:07:39 -0600

    Parcial1 - add pasword

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -1,21 +1,26 @@
 using System.Security.Cryptography;
 using System.Text;
 using api.Data;
+using api.DTOs;
 using api.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers;
 
     public class AccountController(DataContext context) : BaseApiController
     {
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> RegisterAsync(string UserName, string password)
+        public async Task<ActionResult<AppUser>> RegisterAsync(RegisterRequest register)
         {
+            //if (await UserExistAsync(Request.username)){ 
+               // BadRequest("Username alredy in use");
+           // }    
             using var hmac = new HMACSHA512();
-
             var user = new AppUser(){
-            UserName = UserName,
-            passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+
+            UserName = register.UserName,
+            passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(register.Password)),
             passwordSalt = hmac.Key
             };
             context.Users.Add(user);
@@ -23,5 +28,7 @@ namespace api.Controllers;
 
             return user;
         }
+        private async Task<bool> UserExistAsync(string username) =>
+            await context.Users.AnyAsync(u => u.UserName.ToLower() == username.ToLower());
         
     }
