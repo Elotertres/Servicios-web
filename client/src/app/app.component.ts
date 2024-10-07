000d3a4ef3521b07a02b8851a5ac3509f2172301
Juan Pablo Martinez Rocha <Elotertres@hotmail.com>
2024-10-06 20:27:46 -0600

    Parcial1 - add homespace

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,11 +2,14 @@ import { NgFor } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
+import { HomeComponent } from "./home/home.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor],
+  imports: [RouterOutlet, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -20,11 +23,11 @@ export class AppComponent implements OnInit{
     this.getUsers();
     this.setCurrentUser();
   }
-  setCurrentUser(){
+  setCurrentUser(): void{
     const userString = localStorage.getItem("user");
     if(!userString) return ;
     const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
+    this.accountservice.currentUser.set(user);
   }
   getUsers() {
     
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -13,15 +13,14 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown'
 
 })
 export class NavComponent {
-  private accountService = inject(AccountService);
-  loggedIn = false; 
+  accountService = inject(AccountService);
   model: any = {};
 
   login(): void{
     this.accountService.login(this.model).subscribe({
       next: (response) => {
         console.log(response);
-        this.loggedIn = true;
+        
       },
       error: (error) => {
         console.log("Se ha producido un error");
@@ -30,6 +29,7 @@ export class NavComponent {
     })
   }
   logout(): void {
-    this.loggedIn = false; 
+    this.accountService.logout();
+
   }
 }
