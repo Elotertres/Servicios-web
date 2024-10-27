c2f0f031739403a8895e2d583b047981a968abce
Juan Pablo Martinez Rocha <Elotertres@hotmail.com>
2024-10-27 14:43:05 -0600

    Parcial2 - Add Buggy Controller and edit RegisterRequest

--- api/DTOs/registerDto.cs
+++ api/DTOs/registerDto.cs
@@ -5,9 +5,11 @@ namespace api.DTOs;
     public class RegisterRequest
     {
         [Required]
-        public required string Username { get; set; }
+            public string Username { get; set; } = string.Empty;
+
         [Required]
-        public required string Password { get; set; }
+        [StringLength(8, MinimumLength = 4)]
+        public string Password { get; set; } = string.Empty;
     }
     public class LoginRequest
     {
