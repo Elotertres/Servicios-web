76783c5202503aac139a6032bc6cdad79c12b016
Juan Pablo Martinez Rocha <Elotertres@hotmail.com>
2024-10-27 15:42:13 -0600

    Parcial2 - Add not-found and server-error landing pages

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+
 @Component({
   selector: 'app-test-errors',
   standalone: true,
@@ -8,26 +9,32 @@ import { Component, inject } from '@angular/core';
   styleUrl: './test-errors.component.css'
 })
 export class TestErrorsComponent {
+
   baseUrl = "https://localhost:5001/api/";
   private http = inject(HttpClient);
+  validationErrors: string[] = [];
+
   get400Error(): void {
     this.http.get(this.baseUrl + "buggy/bad-request").subscribe({
       next: (response) => console.log(response),
       error: (error) => console.log(error)
     })
   }
+
   get401Error(): void {
     this.http.get(this.baseUrl + "buggy/auth").subscribe({
       next: (response) => console.log(response),
       error: (error) => console.log(error)
     })
   }
+
   get404Error(): void {
     this.http.get(this.baseUrl + "buggy/not-found").subscribe({
       next: (response) => console.log(response),
       error: (error) => console.log(error)
     })
   }
+
   get500Error(): void {
     this.http.get(this.baseUrl + "buggy/server-error").subscribe({
       next: (response) => console.log(response),
@@ -38,7 +45,11 @@ export class TestErrorsComponent {
   get400ValidationError(): void {
     this.http.post(this.baseUrl + "account/register", {}).subscribe({
       next: (response) => console.log(response),
-      error: (error) => console.log(error)
+      error: (error) => {
+        console.log(error);
+        this.validationErrors = error;
+      }
     })
   }
+
 }
