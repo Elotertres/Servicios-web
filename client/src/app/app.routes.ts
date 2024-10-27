8e64bb9eb1577ef46b6e30af26164aa7995c3b9e
Juan Pablo Martinez Rocha <Elotertres@hotmail.com>
2024-10-27 14:09:47 -0600

    Parcial2 - Add dummy route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,13 +4,22 @@ import { MembersDetailComponent } from './members/members-detail/members-detail.
 import { MembersListComponent } from './members/members-list/members-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './_guards/auth.guard';
+
 
 export const routes: Routes = [
     {path:"", component: HomeComponent},
-    {path:"members", component: MembersListComponent},
-    {path:"members/:id", component: MembersDetailComponent},
-    {path:"lists", component: ListsComponent},
-    {path:"messages", component: MessagesComponent},
-    {path:"**", component: HomeComponent, pathMatch: "full"},
+    {
+        path: "",
+        runGuardsAndResolvers: "always",
+        canActivate: [authGuard],
+        children:[
+            {path:"members", component: MembersListComponent, canActivate: [authGuard]},
+            {path:"members/:id", component: MembersDetailComponent},
+            {path:"lists", component: ListsComponent},
+            {path:"messages", component: MessagesComponent}
+        ]
+    },
+    {path:"**", component: HomeComponent, pathMatch: "full"}
     
 ];
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
@@ -16,7 +17,9 @@ import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class NavComponent {
   accountService = inject(AccountService);
-  private router = inject(Router); 
+  private router = inject(Router);
+  private toaster = inject(ToastrService); 
+
   model: any = {};
 
   login(): void{
@@ -26,8 +29,7 @@ export class NavComponent {
         
       },
       error: (error) => {
-        console.log("Se ha producido un error");
-        console.log(error);
+        this.toaster.error(error.error);
       }
     })
   }
