f152f368465dc0798df9776f08604b6e4aca2ddf
Juan Pablo Martinez Rocha <Elotertres@hotmail.com>
2024-10-27 13:58:20 -0600

    Paarcial2 - Add notification service

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations'
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
+
 
 
 import { routes } from './app.routes';
@@ -10,7 +12,10 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideAnimations()
+    provideAnimations(),
+    provideToastr({
+      positionClass: "toast-bottom-right"
+    })
 
   ]
 };
