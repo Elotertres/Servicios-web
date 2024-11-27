d5282feafcea074c3d80cb3190ebd2b1ad4f5643
Juan Pablo Martinez Rocha <Elotertres@hotmail.com>
2024-11-27 13:03:57 -0600

    Parcial3 - Add member card component

--- client/src/app/members/members-list/members-list.component.ts
+++ client/src/app/members/members-list/members-list.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { Member } from '../../models/member';
+import { MemberCardComponent } from "../member-card/member-card.component";
 
 @Component({
-  selector: 'app-members-list',
+  selector: 'app-member-list',
   standalone: true,
-  imports: [],
-  templateUrl: './members-list.component.html',
-  styleUrl: './members-list.component.css'
+
+  imports: [MemberCardComponent,MemberCardComponent],
+  templateUrl: './member-list.component.html',
+  styleUrl: './member-list.component.css'
 })
-export class MembersListComponent {
+
+export class MemberListComponent implements OnInit{
+  private memberService = inject(MembersService);
+  members: Member[] = [];
+  ngOnInit(): void {
+   this.loadMembers();
+  }
+  loadMembers(){
+    this.memberService.getMembers()
+    .subscribe({
+      next: members=>this.members = members
+    })
+  }
 
 }
