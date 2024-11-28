cafb92c067ed248ad68255d144fb19fb429f7419
Juan Pablo Martinez Rocha <Elotertres@hotmail.com>
2024-11-28 17:41:19 -0600

    Parcial3 - Add photo gallery

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,21 +1,27 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideToastr } from 'ngx-toastr';
-import { errorInterceptor } from './_interceptors/error.interceptor';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import { GALLERY_CONFIG, GalleryConfig } from 'ng-gallery';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
+import { errorInterceptor } from './_interceptors/error.interceptor';
 import { jwtInterceptor } from './_interceptors/jwt.interceptor';
-
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([errorInterceptor,jwtInterceptor])),
-    provideAnimations(),
-    provideToastr({
-      positionClass: "toast-bottom-right"
-    })
-
+     provideAnimations(),
+     provideToastr({
+      positionClass: "toast-bottom_right",
+    }),
+    {
+      provide: GALLERY_CONFIG,
+      useValue: {
+        autoHeight: true,
+        imageSize: 'contain'
+      } as GalleryConfig
+    }
   ]
 };
