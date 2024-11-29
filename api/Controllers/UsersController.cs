5d53f40850391be94696048ee67d0941d5a637b8
Juan Pablo Martinez Rocha <Elotertres@hotmail.com>
2024-11-28 18:45:19 -0600

    Parcial3 - Update user repository

--- api/Controllers/UsersController.cs
+++ api/Controllers/UsersController.cs
@@ -1,35 +1,35 @@
 namespace API.Controllers;
 
+using System.Security.Claims;
 using api.Controllers;
-
 using API.Data;
-using API.DataEntities;
 using API.DTOs;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-
 [Authorize]
 public class UsersController : BaseApiController
 {
-
     private readonly IUserRepository _repository;
+        private readonly IMapper _mapper;
+
 
     public UsersController(IUserRepository repository)
     {
         _repository = repository;
     }
 
-    [HttpGet] 
+    [HttpGet]
     public async Task<ActionResult<IEnumerable<MemberResponse>>> GetAllAsync()
     {
-        var members = await _repository.GetAllAsync();
+        var users = await _repository.GetMembersAsync();
 
-        return Ok(members);
+        
+
+        return Ok(users);
     }
-
-    [HttpGet("{username}")] 
+    [HttpGet("{username}")] // api/users/Calamardo
     public async Task<ActionResult<MemberResponse>> GetByUsernameAsync(string username)
     {
         var member = await _repository.GetMemberAsync(username);
@@ -40,5 +40,26 @@ public class UsersController : BaseApiController
         }
 
         return member;
+    }
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateRequest request){
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (username == null)
+        {
+            return BadRequest("No username found in token");
+        }
+
+        var user = await _repository.GetByUsernameAsync(username);
+        if(user == null){
+            return BadRequest("Could not find an user");
+        }
+        _mapper.Map(request, user);
+          _repository.Update(user);
+        if(await _repository.SaveAllAsync())
+        {
+            return NoContent();
+        }
+      
+        return BadRequest("Update user failed");
     }
 }
