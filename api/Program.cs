dda8fb6253ea7a294124c9feb59094bb4492cf86
Juan Pablo Martinez Rocha <Elotertres@hotmail.com>
2024-12-02 02:59:45 -0600

    Parcial3 - Add UNIT TESTS

--- api/Program.cs
+++ api/Program.cs
@@ -1,72 +1,45 @@
+namespace API;
 
 using api.Data;
-using api.Interfaces;
-using api.Services;
-using API.Middlewares;
-using API.Models;
+using API.Data;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>( opt =>
+public class Program
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));    
-});
-
-builder.Services.AddCors();
-
-builder.Services.AddScoped<ITokenService, TokenService>();
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options => 
+    public static async Task Main(string[] args)
     {
-        var tokenKey = builder.Configuration["TokenKey"] ?? throw new ArgumentNullException(nameof(builder.Configuration["TokenKey"]));
-        options.TokenValidationParameters = new TokenValidationParameters
+        var host = CreateHostBuilder(args).Build();
+        using var scope = host.Services.CreateScope();
+        var services = scope.ServiceProvider;
+
+        try
         {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
-            ValidateIssuer = false, 
-            ValidateAudience = false
-        };
-    });
+            var context = services.GetRequiredService<DataContext>();
+            // var userManager = services.GetRequiredService<UserManager<AppUser>>();
+            // var roleManger = services.GetRequiredService<RoleManager<AppRole>>();
 
-var app = builder.Build();
-app.UseMiddleware<ExceptionMiddleware>();
+            await context.Database.MigrateAsync();
+            await Seed.SeedUsersAsync(context); // (userManager, roleManger);
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error has ocurred during migration/seeding");
+        }
 
+        await host.RunAsync();
+    }
 
-// Configure the HTTP request pipeline.
-app.UseCors((cors) => (cors)
-.AllowAnyHeader()
-.AllowAnyMethod()
-.WithOrigins(
-    "http://localhost:4200",
-    "https://localhost:4200"
-));
-
-app.UseAuthorization();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
-try
-{
-    var context = services.GetRequiredService<DataContext>();
-    await context.Database.MigrateAsync();
-    await Seed.SeedUsersAsync(context);
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
-catch (Exception ex)
-{
-    var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex, "An error ocurred during migration/seeding.");
-}
-
-app.Run();
